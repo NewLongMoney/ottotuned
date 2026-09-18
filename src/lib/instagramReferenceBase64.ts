5f7eee6f40aa36c28d85f3cf76e6556a56bed04b
NewLongMoney <wanami33@gmail.com>
2026-09-18 04:55:34 +0300

    Update brand image combiner for 8-part base64

--- src/lib/instagramReferenceBase64.ts
+++ src/lib/instagramReferenceBase64.ts
@@ -1,7 +1,11 @@
-import { BRAND_IMG_PART_0 } from "./brandImgPart0";
-import { BRAND_IMG_PART_1 } from "./brandImgPart1";
-import { BRAND_IMG_PART_2 } from "./brandImgPart2";
-import { BRAND_IMG_PART_3 } from "./brandImgPart3";
+import { BRAND_IMG_P0 } from "./brandImgP0";
+import { BRAND_IMG_P1 } from "./brandImgP1";
+import { BRAND_IMG_P2 } from "./brandImgP2";
+import { BRAND_IMG_P3 } from "./brandImgP3";
+import { BRAND_IMG_P4 } from "./brandImgP4";
+import { BRAND_IMG_P5 } from "./brandImgP5";
+import { BRAND_IMG_P6 } from "./brandImgP6";
+import { BRAND_IMG_P7 } from "./brandImgP7";
 
 export const INSTAGRAM_REFERENCE_JPEG_BASE64 =
-  BRAND_IMG_PART_0 + BRAND_IMG_PART_1 + BRAND_IMG_PART_2 + BRAND_IMG_PART_3;
+  BRAND_IMG_P0 + BRAND_IMG_P1 + BRAND_IMG_P2 + BRAND_IMG_P3 + BRAND_IMG_P4 + BRAND_IMG_P5 + BRAND_IMG_P6 + BRAND_IMG_P7;
