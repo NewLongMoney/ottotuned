1602b53b49e291e3d3a252ffff23e96a5e6068ad
NewLongMoney <wanami33@gmail.com>
2026-09-18 04:56:11 +0300

    Update brand image combiner for 16-part base64

--- src/lib/instagramReferenceBase64.ts
+++ src/lib/instagramReferenceBase64.ts
@@ -1,11 +1,19 @@
-import { BRAND_IMG_P0 } from "./brandImgP0";
-import { BRAND_IMG_P1 } from "./brandImgP1";
-import { BRAND_IMG_P2 } from "./brandImgP2";
-import { BRAND_IMG_P3 } from "./brandImgP3";
-import { BRAND_IMG_P4 } from "./brandImgP4";
-import { BRAND_IMG_P5 } from "./brandImgP5";
-import { BRAND_IMG_P6 } from "./brandImgP6";
-import { BRAND_IMG_P7 } from "./brandImgP7";
+import { B0 } from "./b0";
+import { B1 } from "./b1";
+import { B2 } from "./b2";
+import { B3 } from "./b3";
+import { B4 } from "./b4";
+import { B5 } from "./b5";
+import { B6 } from "./b6";
+import { B7 } from "./b7";
+import { B8 } from "./b8";
+import { B9 } from "./b9";
+import { B10 } from "./b10";
+import { B11 } from "./b11";
+import { B12 } from "./b12";
+import { B13 } from "./b13";
+import { B14 } from "./b14";
+import { B15 } from "./b15";
 
 export const INSTAGRAM_REFERENCE_JPEG_BASE64 =
-  BRAND_IMG_P0 + BRAND_IMG_P1 + BRAND_IMG_P2 + BRAND_IMG_P3 + BRAND_IMG_P4 + BRAND_IMG_P5 + BRAND_IMG_P6 + BRAND_IMG_P7;
+  B0 + B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9 + B10 + B11 + B12 + B13 + B14 + B15;
